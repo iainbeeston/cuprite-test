f04cea2a0571aca9ee78b045c9e6149bcde55440
Iain Beeston <iainbeeston@users.noreply.github.com>
2019-01-25 17:06:59 +0000

    Added an additional build using selenium

--- spec/system/cuprite_spec.rb
+++ spec/system/cuprite_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Cuprite', type: :system do
+  before do
+    driven_by :cuprite
+  end
+
   it 'loads the page' do
     visit 'pages/home'
 
@@ -11,5 +15,23 @@ RSpec.describe 'Cuprite', type: :system do
     visit 'pages/home'
 
     expect(find('tt')).to have_text('Hello from react-rails')
+  end
+
+  context 'using selenium' do
+    before do
+      driven_by :selenium_chrome_headless
+    end
+
+    it 'loads the page' do
+      visit 'pages/home'
+
+      expect(page).to have_text('Find me in app/views/pages/home.html.erb')
+    end
+
+    it 'renders a react component' do
+      visit 'pages/home'
+
+      expect(find('tt')).to have_text('Hello from react-rails')
+    end
   end
 end
